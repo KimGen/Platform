398ffdf1d0030c536e5b34f990ccb5e559f2a60e
cristophernic <cristophernic@live.cl>
2016-11-18 19:25:14 -0300

    remove sudo for container

--- _one-click-installation/bootstrap.sh
+++ _one-click-installation/bootstrap.sh
@@ -5,27 +5,27 @@ PASSWORD='12345678'
 PROJECTFOLDER='platform'
 
 # Create project folder, written in 3 single mkdir-statements to make sure this runs everywhere without problems
-sudo mkdir "/var/www"
-sudo mkdir "/var/www/html"
-sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+mkdir "/var/www"
+mkdir "/var/www/html"
+mkdir "/var/www/html/${PROJECTFOLDER}"
 
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
-sudo apt-get install -y apache2
-sudo apt-get install -y php5
+apt-get install -y apache2
+apt-get install -y php5
 
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+apt-get -y install mysql-server
+apt-get install php5-mysql
 
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+apt-get -y install phpmyadmin
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -41,29 +41,29 @@ EOF
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
 # enable mod_rewrite
-sudo a2enmod rewrite
+a2enmod rewrite
 
 # restart apache
-service apache2 restart
+apache2 restart
 
 # install curl (needed to use git afaik)
-sudo apt-get -y install curl
-sudo apt-get -y install php5-curl
+apt-get -y install curl
+apt-get -y install php5-curl
 
 # install openssl (needed to clone from GitHub, as github is https only)
-sudo apt-get -y install openssl
+apt-get -y install openssl
 
 # install PHP GD, the graphic lib (we create captchas and avatars)
-sudo apt-get -y install php5-gd
+apt-get -y install php5-gd
 
 # install git
-sudo apt-get -y install git
+apt-get -y install git
 
 # git clone HUGE
-sudo git clone https://github.com/KimGen/platform "/var/www/html/${PROJECTFOLDER}"
+git clone https://github.com/KimGen/platform "/var/www/html/${PROJECTFOLDER}"
 
 # install Composer
-curl -s https://getcomposer.org/installer | php
+wget -s https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # go to project folder, load Composer packages
@@ -71,15 +71,15 @@ cd "/var/www/html/${PROJECTFOLDER}"
 composer install
 
 # run SQL statements from install folder
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/application/_installation/01-create-database.sql"
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/application/_installation/02-create-table-users.sql"
-sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/application/_installation/03-create-table-notes.sql"
+mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/application/_installation/01-create-database.sql"
+mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/application/_installation/02-create-table-users.sql"
+mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/application/_installation/03-create-table-notes.sql"
 
 # writing rights to avatar folder
-sudo chmod 0777 -R "/var/www/html/${PROJECTFOLDER}/public/avatars"
+chmod 0777 -R "/var/www/html/${PROJECTFOLDER}/public/avatars"
 
 # remove Apache's default demo file
-sudo rm "/var/www/html/index.html"
+rm "/var/www/html/index.html"
 
 # final feedback
 echo "Voila!"
