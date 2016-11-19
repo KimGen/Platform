7e8988423127295f38e5b4300e48404872939fe3
cristophernic <cristophernic@live.cl>
2016-11-19 09:22:22 -0300

    update unix script

--- _one-click-installation/bootstrap.sh
+++ _one-click-installation/bootstrap.sh
@@ -9,20 +9,10 @@ mkdir "/var/www"
 mkdir "/var/www/html"
 mkdir "/var/www/html/${PROJECTFOLDER}"
 
-apt-get install -y apache2
-apt-get install -y php5
-
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 apt-get -y install mysql-server
 apt-get install php5-mysql
-
-debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-apt-get -y install phpmyadmin
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -43,17 +33,8 @@ a2enmod rewrite
 # restart apache
 apache2 restart
 
-# install curl (needed to use git afaik)
-apt-get -y install curl php5-curl
-
-# install openssl (needed to clone from GitHub, as github is https only)
-apt-get -y install openssl
-
 # install PHP GD, the graphic lib (we create captchas and avatars)
 apt-get -y install php5-gd
-
-# install git
-apt-get -y install git
 
 # git clone HUGE
 git clone https://github.com/KimGen/platform "/var/www/html/${PROJECTFOLDER}"
